b3ccd72a3aee0cc07275ad3b667d615bd5d143b9
Trevor Jacobs <117475995+tjacobs2802@users.noreply.github.com>
2025-05-19 00:07:55 +0000

    Added all of the files neede, got headshot image and tailwind installed

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import headshot from './assets/jacobshead.png'
 import './App.css'
 
 function App() {
@@ -9,12 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={headshot} className="logo" alt="headshot" />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
